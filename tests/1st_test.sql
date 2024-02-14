e7138d1cfc9af55f1c31ad6ad8e5ff71ee54a750
Elementary Data <team@elementary-data.com>
2024-02-14 18:49:59 +0000

    Create a custom SQL test.

---
+++ tests/1st_test.sql
@@ -0,0 +1,9 @@
+{{
+    config(
+        meta={'owner': ['o1', 'o2'], 'description': 'My new test generated using Elementary.'},
+        tags=['#tagged'],
+        severity=error
+    )
+}}
+
+SELECT 1 AS one
