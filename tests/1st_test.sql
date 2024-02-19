fb0ce0fd6ec0a22180a6ef870f9353def23a90ed
Elementary Data <team@elementary-data.com>
2024-02-19 15:12:59 +0000

    Delete a custom SQL test.

--- tests/1st_test.sql
+++
@@ -1,9 +0,0 @@
-{{
-    config(
-        meta={'owner': ['o1', 'o2'], 'description': 'My new test generated using Elementary.'},
-        severity='error',
-        tags=['#tagged']
-    )
-}}
-
-SELECT 1 AS one
--- tests/2nd_test.sql
+++
@@ -1,7 +0,0 @@
-{{
-    config(
-        meta={'owner': ['o1'], 'description': 'bla'}
-    )
-}}
-
-SELECT 2 AS two
