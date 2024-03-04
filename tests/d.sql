7bf2f367ff70d4e0b746a3589b49af28fc430a8a
Elementary Data <team@elementary-data.com>
2024-03-04 12:43:53 +0000

    Create a custom SQL test.

---
+++ tests/d.sql
@@ -0,0 +1,9 @@
+{{
+    config(
+        override_primary_test_model_id='source.elementary_tutorial.date.date',
+        severity='error',
+        meta={'owner': ['noy@elementary-data.com']}
+    )
+}}
+
+sss
